9d98a21489318f2554059dc8e315b5dfb7f51e1e
everestwilkey <themount707@gmail.com>
2025-09-26 21:23:08 -0600

    Add Header component and integrate Bootstrap into project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header }  from '../app/header/header'
+import {Contacts} from '../app/contacts/contacts'
 
 @Component({
   selector: 'cms-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Contacts],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
